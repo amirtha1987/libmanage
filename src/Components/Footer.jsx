fe41ae4d7431dbee384b08ffddcea3fb822d2e7e
amirthavalli1987 <amirtha.dit@gmail.com>
2024-09-09 23:08:09 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-3">
+      <div className="container">
+        <p className="mb-0">© {new Date().getFullYear()} </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
